75d4298fe01c5a394cbef4022380f7ac03dc2edf
steveeakin <steven.eakin@gmail.com>
2015-09-11 14:00:46 -0400

    Stabalizing for L5

--- src/config/amazon-mws.php
+++ src/config/amazon-mws.php
@@ -3,15 +3,15 @@
 return [
 	'store' => [
 		'drhittich' => [
-			'merchantId' => '',
-			'marketplaceId' => '',
-			'keyId' => '',
-			'secretKey' => ''
+			'merchantId' => 'A2D528YMGU7753',
+			'marketplaceId' => 'ATVPDKIKX0DER',
+			'keyId' => 'AKIAJ4IRZZEXYVMOGVCA',
+			'secretKey' => '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'
 		]
 	],
 
 	//Service URL Base
-	'AMAZON_SERVICE_URL' => 'https://mws-eu.amazonservices.com/',
+	'AMAZON_SERVICE_URL' => 'https://mws.amazonservices.com/',
 
 	'muteLog' => false
 ];
